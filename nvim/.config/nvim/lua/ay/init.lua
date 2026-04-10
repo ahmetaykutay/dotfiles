c2dd26b8709f4627aeb44b0030b8d95435bfbac6
Ahmet Aykut Ay <ahmetaykutay@gmail.com>
2026-04-10 14:34:28 +0200

    quickfix window fix

--- nvim/.config/nvim/lua/ay/init.lua
+++ nvim/.config/nvim/lua/ay/init.lua
@@ -4,3 +4,11 @@ require('ay.settings')
 
 vim.cmd.colorscheme "catppuccin-macchiato"
 
+-- This makes the Enter key actually open the file in the Quickfix window
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.keymap.set("n", "<CR>", "<CR>:cclose<CR>", { buffer = true, silent = true })
+  end,
+})
+
