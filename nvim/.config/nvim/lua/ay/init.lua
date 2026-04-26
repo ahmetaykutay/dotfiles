b5942519247a3acf6df46033aaa9f58ff17997a3
Ahmet Aykut Ay <ahmetaykutay@gmail.com>
2026-04-26 18:20:56 +0200

    coreui2

--- nvim/.config/nvim/lua/ay/init.lua
+++ nvim/.config/nvim/lua/ay/init.lua
@@ -1,20 +1,22 @@
-require('ay.lazy')
-require('ay.remap')
-require('ay.settings')
-require('ay.harpoon')
+require("vim._core.ui2").enable({})
 
-vim.cmd.colorscheme "catppuccin-macchiato"
+require("ay.lazy")
+require("ay.remap")
+require("ay.settings")
+require("ay.harpoon")
+
+vim.cmd.colorscheme("catppuccin-macchiato")
 
 -- This makes the Enter key actually open the file in the Quickfix window
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.keymap.set("n", "<CR>", "<CR>:cclose<CR>", { buffer = true, silent = true })
-  end,
+	pattern = "qf",
+	callback = function()
+		vim.keymap.set("n", "<CR>", "<CR>:cclose<CR>", { buffer = true, silent = true })
+	end,
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    pcall(vim.treesitter.start)
-  end
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
 })
